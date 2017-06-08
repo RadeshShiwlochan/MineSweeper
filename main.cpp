9af73b1a44826a5c28e63ada3c81e2fe43df243c
radeshshiwlochan <radesh0430@gmail.com>
2017-06-08 01:12:25 -0400

    prints correct result prior testing

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-void printField(char** arr, int rows, int cols) {
-	
+void printField(string** arr, int rows, int cols) {
+
 	for(int r = 0; r < rows + 2; ++r) {
 		for(int c = 0; c < cols + 2; ++c) {
 			cout << arr[r][c] << "  ";
@@ -13,15 +13,48 @@ void printField(char** arr, int rows, int cols) {
 	}
 }
 
-void addFrame(char** arr, int rows, int cols)  {
+void addFrame(string** arr, int rows, int cols)  {
 
 	int rowSize = rows + 2; 
 	int colSize = cols + 2;
 	for(int i = 0; i < rowSize; ++i) {
 		for(int j = 0; j < colSize; ++j) {
-			arr[i][j] = '#';
+			arr[i][j] = "#";
 		}
 	}
+}
+
+void countAdjMines(string** arr, int rows, int cols) {
+	int count = 0;
+	int tempVal = 0;
+	string value = "";
+	for(int r = 1; r < rows; ++r) {
+		for(int c = 1; c < cols; ++c) {
+			if(arr[r][c] != "*") {
+				if(arr[r - 1][c - 1] == "*" )
+					count++;
+				if(arr[r - 1][c] == "*") 
+					count++;
+				if(arr[r - 1][c + 1] == "*") 
+					count++;
+				if(arr[r][c - 1] == "*") 
+					count++;
+				if(arr[r][c + 1] == "*")
+					count++;
+				if(arr[r + 1][c - 1] == "*")
+					count++;
+				if(arr[r + 1][c] == "*")
+					count++;
+				if(arr[r + 1][c + 1] == "*")
+					count++;
+				tempVal = count;
+				value = to_string(tempVal);
+				arr[r][c] = value;
+				count = 0;
+			}//if
+
+		}//for columns
+	}//for rows
 }
 
 void findMines(string inputFile) {
@@ -32,11 +65,11 @@ void findMines(string inputFile) {
 	int columns = 0;
 	char value;
 	readInput >> rows >> columns;
-	char** field;
+	string** field;
 
-	field = new char*[rows + 2];
+	field = new string*[rows + 2];
 	for(int i = 0; i < rows + 2; ++i)
-		field[i] = new char[columns + 2]();
+		field[i] = new string[columns + 2]();
 
 	addFrame(field, rows, columns);
 	for(int r = 1; r < rows + 1; ++r) {
@@ -45,6 +78,7 @@ void findMines(string inputFile) {
 			field[r][c] = value;
 		}
 	}
+	countAdjMines(field, rows + 1, columns + 1);
 	printField(field, rows, columns);
 }
 
@@ -54,7 +88,6 @@ int main(int argc, char* argv[]) {
 		cout << "Needs an inputFile, Terminating\n";
 		return 0;
 	}
-
 	findMines(argv[1]);
 	return 0;
 }
